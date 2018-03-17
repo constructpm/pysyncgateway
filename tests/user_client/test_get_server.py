1a732a52371060151c878853b7798de7b2fe0935
James Cooke <jamescooke@users.noreply.github.com>
2018-03-17 16:44:20 +0000

    Extend test for server info on public API

--- tests/user_client/test_get_server.py
+++ tests/user_client/test_get_server.py
@@ -2,10 +2,10 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from pysyncgateway import UserClient
 
-# --- FAILURES ---
 
-
-def test_no_auth(syncgateway_public_url):
+def test(syncgateway_public_url, existing_user):
     user_client = UserClient(syncgateway_public_url)
 
-    user_client.get_server()
+    result = user_client.get_server()
+
+    assert sorted(list(result)) == ['couchdb', 'vendor', 'version']  # No ADMIN key
