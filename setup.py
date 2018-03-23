83cb336cdd07aada65e2a5ec4b6aeaf802fee48f
James Cooke <jamescooke@users.noreply.github.com>
2018-03-23 13:57:20 +0000

    Update README and include in setup.py long_description

--- setup.py
+++ setup.py
@@ -2,14 +2,21 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from setuptools import setup
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(
     name='pysyncgateway',
     version='0.1.0',
     description='Library for communication with Couchbase Sync Gateway',
+    long_description=readme(),
     url='https://github.com/constructpm/pysyncgateway',
     author='James Cooke',
     author_email='github@jamescooke.info',
-    license="http://www.apache.org/licenses/LICENSE-2.0",
+    license='Apache License 2.0',
     packages=['pysyncgateway'],
     zip_safe=False,
     classifiers=[
