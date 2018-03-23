d91ac41db4853297dbffba51471b1b9cad1be3cf
James Cooke <jamescooke@users.noreply.github.com>
2018-03-23 11:27:43 +0000

    Fix ambiguous test (RED on tox), mark pkg as known 3rd party for isort

--- tests/admin_client/test_get_server.py
+++ tests/admin_client/test_get_server.py
@@ -5,6 +5,11 @@ def test(admin_client):
     result = admin_client.get_server()
 
     assert isinstance(result, dict)
-    assert list(result) == ['ADMIN', 'couchdb', 'vendor', 'version']
+    assert sorted(list(result)) == [
+        'ADMIN',
+        'couchdb',
+        'vendor',
+        'version',
+    ]
     assert result['ADMIN'] is True
     assert result['version'] == 'Couchbase Sync Gateway/1.5.1(4;cb9522c)'
