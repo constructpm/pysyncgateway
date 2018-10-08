1c61401457e429420626e9a33416730c15721091
James Cooke <jamescooke@users.noreply.github.com>
2018-10-08 17:42:07 +0100

    Extract document fixtures to conftest for reuse, add a conflicted doc

--- tests/document/test_fixtures.py
+++ tests/document/test_fixtures.py
@@ -12,3 +12,24 @@ def test_empty_document(empty_document):
 
     assert result.data == {}
     assert result.rev == ''
+
+
+def test_recipe_document(recipe_document, database):
+    result = recipe_document
+
+    assert result in database.all_docs()
+
+
+def test_permissions_document(permissions_document, database):
+    result = permissions_document
+
+    assert result in database.all_docs()
+
+
+def test_conflicted_document(conflicted_document, database):
+    result = conflicted_document
+
+    assert database.all_docs() == [result]
+    assert result.rev == '1-123'
+    assert result.retrieve()
+    assert result.rev == '1-789'
