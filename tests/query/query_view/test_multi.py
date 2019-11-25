8164cf967832704539f026e78bc52ebb5d7af8ce
Mike Saysell <michael.saysell@biositesystems.com>
2019-11-25 16:38:50 +0000

    fix - More lint fixes

--- tests/query/query_view/test_multi.py
+++ tests/query/query_view/test_multi.py
@@ -1,4 +1,5 @@
 import uuid
+
 import pytest
 
 
@@ -24,30 +25,30 @@ def schools_database(database):
     """
     teacher_doc = database.get_document(uuid.uuid4().hex)
     teacher_doc.data = {
-        'type': 'teacher',
-        'school': 'Olives',
+        "type": "teacher",
+        "school": "Olives",
     }
     teacher_doc.create_update()
     for name, parent, school in [
-        ('Amy', None, None),
-        ('Andy', 'Paul', 'Olives'),
-        ('Chris', None, 'St Barts'),
-        ('Jo', 'Mel', 'St Barts'),
-        ('Maggy', 'Mel', 'St Barts'),
-        ('Mary', 'Maggy', None),
+        ("Amy", None, None),
+        ("Andy", "Paul", "Olives"),
+        ("Chris", None, "St Barts"),
+        ("Jo", "Mel", "St Barts"),
+        ("Maggy", "Mel", "St Barts"),
+        ("Mary", "Maggy", None),
     ]:
         doc = database.get_document(uuid.uuid4().hex)
         doc.data = {
-            'type': 'pupil',
-            'name': name,
-            'parent': parent,
-            'school': school,
+            "type": "pupil",
+            "name": name,
+            "parent": parent,
+            "school": school,
         }
         doc.create_update()
     class_doc = database.get_document(uuid.uuid4().hex)
     class_doc.data = {
-        'type': 'class',
-        'subject': 'Geography',
+        "type": "class",
+        "subject": "Geography",
     }
     class_doc.create_update()
     return database
@@ -59,9 +60,11 @@ def test_schools_database(schools_database):
     all_docs = result.all_docs()
     assert len(all_docs) == 8
     [doc.retrieve() for doc in all_docs]
-    not_at_school = [doc for doc in all_docs if 'school' in doc.data and doc.data['school'] is None]
+    not_at_school = [
+        doc for doc in all_docs if "school" in doc.data and doc.data["school"] is None
+    ]
     assert len(not_at_school) == 2
-    assert sorted([doc.data['name'] for doc in not_at_school]) == ['Amy', 'Mary']
+    assert sorted([doc.data["name"] for doc in not_at_school]) == ["Amy", "Mary"]
 
 
 @pytest.fixture
@@ -71,12 +74,11 @@ def pupil_data_design_doc(database):
         Query: 'pupil_data' design document containing a single view called
         'list_pupils' which is keyed with `(parent, school)`.
     """
-    query = database.get_query('pupil_data')
+    query = database.get_query("pupil_data")
     query.data = {
-        'views': {
-            'list_pupils': {
-                'map':
-                """
+        "views": {
+            "list_pupils": {
+                "map": """
 function (doc,meta) {
     if(doc.type == "pupil") {
         emit([doc.parent, doc.school], doc);
@@ -91,16 +93,16 @@ function (doc,meta) {
 
 
 def test_pupil_query_doc_empty(pupil_data_design_doc):
-    result = pupil_data_design_doc.query_view('list_pupils')
+    result = pupil_data_design_doc.query_view("list_pupils")
 
-    assert result['total_rows'] == 0
+    assert result["total_rows"] == 0
 
 
 def test_pupil_query_doc_with_data(pupil_data_design_doc, schools_database):
-    result = pupil_data_design_doc.query_view('list_pupils')
+    result = pupil_data_design_doc.query_view("list_pupils")
 
-    assert result['total_rows'] == 6
-    keys = [row['key'] for row in result['rows']]
+    assert result["total_rows"] == 6
+    keys = [row["key"] for row in result["rows"]]
     assert all([len(k) == 2 for k in keys])
 
 
@@ -108,35 +110,36 @@ def test_pupil_query_doc_with_data(pupil_data_design_doc, schools_database):
 
 
 @pytest.mark.parametrize(
-    'keys, expected_names',
+    "keys, expected_names",
     [
-        (['Mel', 'St Barts'], ['Jo', 'Maggy']),
-        ((None, 'St Barts'), ['Chris']),  # Uses a tuple of keys
-        (['Maggy', None], ['Mary']),
-        ([None, None], ['Amy']),
-    ]
+        (["Mel", "St Barts"], ["Jo", "Maggy"]),
+        ((None, "St Barts"), ["Chris"]),  # Uses a tuple of keys
+        (["Maggy", None], ["Mary"]),
+        ([None, None], ["Amy"]),
+    ],
 )
 def test_both_keys(keys, expected_names, pupil_data_design_doc, schools_database):
-    result = pupil_data_design_doc.query_view('list_pupils', key=keys)
+    result = pupil_data_design_doc.query_view("list_pupils", key=keys)
 
-    assert result['total_rows'] == len(expected_names)
-    assert sorted([row['value']['name'] for row in result['rows']]) == expected_names
+    assert result["total_rows"] == len(expected_names)
+    assert sorted([row["value"]["name"] for row in result["rows"]]) == expected_names
 
 
 @pytest.mark.parametrize(
-    'keys', [
-        ('Mel', []),
-        ([], 'Olives'),
-        ([{}, 'St Barts'], []),
-        ([(), 'St Barts'], []),
-        ([[], 'St Barts'], []),
-    ]
+    "keys",
+    [
+        ("Mel", []),
+        ([], "Olives"),
+        ([{}, "St Barts"], []),
+        ([(), "St Barts"], []),
+        ([[], "St Barts"], []),
+    ],
 )
 def test_bad(keys, pupil_data_design_doc, schools_database):
     """
     Pinning behaviour of badly formed keys - all give no data and no error.
     """
-    result = pupil_data_design_doc.query_view('list_pupils', key=keys)
+    result = pupil_data_design_doc.query_view("list_pupils", key=keys)
 
-    assert result['total_rows'] == 0
-    assert result['rows'] == []
+    assert result["total_rows"] == 0
+    assert result["rows"] == []
